bd50da6cc7898c1cb7dccc4bccbb244d4026c841
Thomas Guiu <thomas.guiu@soyatec.com>
2017-02-27 11:09:55 +0100

    [1475] Update metadata metamodel
    Bug: 1475
    
    Change-Id: I17caf6f2aecc429a06012bfcbe6a1b3b069f4cc4
    Signed-off-by: Thomas Guiu <thomas.guiu@soyatec.com>

--- architecture description/metadata/plugins/org.polarsys.kitalpha.ad.metadata.model/generated/org/polarsys/kitalpha/ad/metadata/metadata/util/MetadataResourceImpl.java
+++ architecture description/metadata/plugins/org.polarsys.kitalpha.ad.metadata.model/generated/org/polarsys/kitalpha/ad/metadata/metadata/util/MetadataResourceImpl.java
@@ -1,6 +1,6 @@
 /**
  * ******************************************************************************
- *  Copyright (c) 2016 Thales Global Services S.A.S.
+ *  Copyright (c) 2016-2017 Thales Global Services S.A.S.
  *  All rights reserved. This program and the accompanying materials
  *  are made available under the terms of the Eclipse Public License v1.0
  *  which accompanies this distribution, and is available at
@@ -17,7 +17,6 @@
 package org.polarsys.kitalpha.ad.metadata.metadata.util;
 
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.emf.ecore.xmi.impl.XMIResourceImpl;
 
 /**
